307d622006191f7547756f8861e6cdd95c96207c
Stéfano Araujo <beingsane@gmail.com>
2018-02-16 11:31:17 -0200

    Update ngc-editor-html-component.js

--- component/ngc-editor-html-component.js
+++ component/ngc-editor-html-component.js
@@ -1,1 +1,24 @@
-
+;(function () {
+    'use strict';
+  
+    /**
+     * @module application.ngEditorHTML
+     */
+    angular
+      .module('application.ngEditorHTML.component')
+      .component('ngcEditorHtml', {
+        controller: 'ngcEditorHtmlController',
+        controllerAs: 'vm',
+        templateUrl: 'ngEditorHTML/view/ngc-editor-html-view.html',
+        require: {
+        },
+        bindings: {
+          label: '@',
+          modelo: '@',
+          tamanho: '<',
+          descricao: '@',
+          editavel: '<',
+          ngModel: '=',
+        }
+      });
+}());
